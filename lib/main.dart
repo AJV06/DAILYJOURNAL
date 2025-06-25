6db821b7f593dbcb3af2309c5a298a844cc2f1a5
AJV06 <adwaithajv@gmail.com>
2025-06-25 17:32:16 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'SCREEN/homescreen.dart';
-import 'SCREEN/homescreen.dart';
 
 void main() {
   runApp(const DailyJournalApp());
